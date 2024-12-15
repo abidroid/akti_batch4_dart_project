ae1647e90ef8fc2740b1466c45a7b371eb08415d
Abid <abidroid1@gmail.com>
2024-12-15 11:02:51 +0500

    multiple if

--- bin/akti_batch4_dart_project.dart
+++ bin/akti_batch4_dart_project.dart
@@ -10,17 +10,49 @@
 import 'dart:io';
 void main()
 {
-  int n;
-  stdout.write("Enter a number less than 10 : ");
-  n = int.parse( stdin.readLineSync()!);
+  // int n;
+  // stdout.write("Enter a number less than 10 : ");
+  // n = int.parse( stdin.readLineSync()!);
+  //
+  // if( n > 10 )
+  // {
+  //   print('Very Bad');
+  // }
+  // else
+  // {
+  //   print('Shaabaash');
+  // }
 
-  if( n > 10 )
-  {
-    print('Very Bad');
+
+  int m;
+  stdout.write("Enter month number : ");
+  m = int.parse( stdin.readLineSync()!);
+
+  if( m == 1 ){
+    print('January');
   }
-  else
-  {
-    print('Shaabaash');
+
+  if( m == 2 ){
+    print("February");
+  }
+
+  if( m == 3 ) {
+    print("March");
+  }
+
+  // Add the missing conditions
+
+  if( m == 12 ) {
+    print('December');
+  }
+
+  if( m > 12 ){
+    print('Invalid');
+  }
+
+  if( m < 1 ){
+    print('Invalid');
+
   }
 }
 
