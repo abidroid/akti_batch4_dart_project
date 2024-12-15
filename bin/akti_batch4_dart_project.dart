d018e4e49f65bd08e36d45150009d3395610b76d
Abid <abidroid1@gmail.com>
2024-12-15 11:25:05 +0500

    calc using multiple if

--- bin/akti_batch4_dart_project.dart
+++ bin/akti_batch4_dart_project.dart
@@ -24,34 +24,77 @@ void main()
   // }
 
 
-  int m;
-  stdout.write("Enter month number : ");
-  m = int.parse( stdin.readLineSync()!);
+  // int m;
+  // stdout.write("Enter month number : ");
+  // m = int.parse( stdin.readLineSync()!);
+  //
+  // if( m == 1 ){
+  //   print('January');
+  // }
+  //
+  // if( m == 2 ){
+  //   print("February");
+  // }
+  //
+  // if( m == 3 ) {
+  //   print("March");
+  // }
+  //
+  // // Add the missing conditions
+  //
+  // if( m == 12 ) {
+  //   print('December');
+  // }
+  //
+  // if( m > 12 ){
+  //   print('Invalid');
+  // }
+  //
+  // if( m < 1 ){
+  //   print('Invalid');
+  //
+  // }
 
-  if( m == 1 ){
-    print('January');
+  int n1, n2, choice;
+  stdout.write('Enter first number : ');
+  n1 = int.parse( stdin.readLineSync()! );
+
+  stdout.write('Enter second number : ');
+  n2 = int.parse( stdin.readLineSync()! );
+
+  String message = '''
+Select Operation
+1. Add
+2. Sub
+3. Mul
+4. Div
+Your choice ? ''';
+
+  stdout.write(message);
+  choice = int.parse(stdin.readLineSync()!);
+
+  if( choice == 1 ){
+    print('Sum = ${n1 + n2}');
   }
 
-  if( m == 2 ){
-    print("February");
+  if( choice == 2 ){
+    print('Difference = ${n1-n2}');
   }
 
-  if( m == 3 ) {
-    print("March");
+  if( choice == 3){
+    print('Product = ${n1 * n2 }');
   }
 
-  // Add the missing conditions
-
-  if( m == 12 ) {
-    print('December');
+  if( choice == 4 ){
+    print('Quotient = ${n1 / n2}');
   }
 
-  if( m > 12 ){
-    print('Invalid');
+  if( choice > 4 ) {
+    print('Invalid operation code');
   }
 
-  if( m < 1 ){
-    print('Invalid');
+  if( choice < 1 ){
+    print('Invalid operation code');
 
   }
 }
