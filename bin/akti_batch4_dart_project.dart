2c0d7fd85e5ac97f7cb99636a1fd3ea0dc2ffbb7
Abid <abidroid1@gmail.com>
2024-12-21 11:21:02 +0500

    maps

--- bin/akti_batch4_dart_project.dart
+++ bin/akti_batch4_dart_project.dart
@@ -11,29 +11,63 @@ import 'dart:io';
 void main() {
 
   // Writing
-  List<String> studentList = [
-    "Ali",
-    "Bilal",
-    "Khan",
-    "Jamal",
-  ];
+  // List<String> studentList = [
+  //   "Ali",
+  //   "Bilal",
+  //   "Khan",
+  //   "Jamal",
+  // ];
+  //
+  // studentList.add("Haris");
+  // studentList.add("Hina");
+  //
+  // // Traversing - accessing the elements fo the list
+  //
+  // // Reading
+  // for( int i = 0; i < studentList.length; i++ ){
+  //   print(studentList[i]);
+  // }
+  //
+  // // for in loop - for each loop
+  // // used with list
+  // print('****************************');
+  //
+  // for( String s in studentList){
+  //   print(s);
+  // }
 
-  studentList.add("Haris");
-  studentList.add("Hina");
+  // Map
+  // Key Value pairs
+  // Map<String, int> playerJersey = {
+  //   'afridi': 10,
+  //   'kohli': 18,
+  //   'babar': 56,
+  //   'messi': 99,
+  //   'khan': 88,
+  // };
+  //
+  // playerJersey['saim'] = 19;
+  // playerJersey['babar'] = 58; // update
+  //
+  // print(playerJersey);
 
-  // Traversing - accessing the elements fo the list
+  Map<String, dynamic> bilalInfo = {
+    'rollNo': 1,
+    'name': "Bilal",
+    'gpa': 3.3,
+    'isMarried': false,
+    'friends': ['khan', 'gul jan'],
+    'address': {
+      'houseNo': 23,
+      'streetNo': 4,
+      'city': "Peshawar",
+    },
+  };
 
-  // Reading
-  for( int i = 0; i < studentList.length; i++ ){
-    print(studentList[i]);
-  }
+  print(bilalInfo['address']['streetNo']);
 
-  // for in loop - for each loop
-  // used with list
-  print('****************************');
 
-  for( String s in studentList){
-    print(s);
-  }
+  print(bilalInfo.keys);
+  print(bilalInfo.values);
 
 }
