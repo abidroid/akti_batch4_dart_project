071f109465fb573d789be93197bb0b7e0f46f764
Abid <abidroid1@gmail.com>
2024-12-21 11:00:49 +0500

    list

--- bin/akti_batch4_dart_project.dart
+++ bin/akti_batch4_dart_project.dart
@@ -6,95 +6,34 @@
   Program
  */
 
-
 import 'dart:io';
-void main()
-{
-  int n;
-  stdout.write("Enter a number less than 10 : ");
-  n = int.parse( stdin.readLineSync()!);
 
-  (n > 10) ? print('very bad') : print("shaabash");
+void main() {
 
+  // Writing
+  List<String> studentList = [
+    "Ali",
+    "Bilal",
+    "Khan",
+    "Jamal",
+  ];
 
-  // int m;
-  // stdout.write("Enter month number : ");
-  // m = int.parse( stdin.readLineSync()!);
-  //
-  // if( m == 1 ){
-  //   print('January');
-  // }
-  //
-  // if( m == 2 ){
-  //   print("February");
-  // }
-  //
-  // if( m == 3 ) {
-  //   print("March");
-  // }
-  //
-  // // Add the missing conditions
-  //
-  // if( m == 12 ) {
-  //   print('December');
-  // }
-  //
-  // if( m > 12 ){
-  //   print('Invalid');
-  // }
-  //
-  // if( m < 1 ){
-  //   print('Invalid');
-  //
-  // }
+  studentList.add("Haris");
+  studentList.add("Hina");
 
-//   int n1, n2, choice;
-//   stdout.write('Enter first number : ');
-//   n1 = int.parse( stdin.readLineSync()! );
-//
-//   stdout.write('Enter second number : ');
-//   n2 = int.parse( stdin.readLineSync()! );
-//
-//   String message = '''
-// Select Operation
-// 1. Add
-// 2. Sub
-// 3. Mul
-// 4. Div
-// Your choice ? ''';
-//
-//   stdout.write(message);
-//   choice = int.parse(stdin.readLineSync()!);
-//
-//   if( choice == 1 ){
-//     print('Sum = ${n1 + n2}');
-//   }
-//   else if( choice == 2 ){
-//     print('Difference = ${n1-n2}');
-//   }
-//   else if( choice == 3){
-//     print('Product = ${n1 * n2 }');
-//   }
-//   else if( choice == 4 ){
-//     print('Quotient = ${n1 / n2}');
-//   }
-//   else {
-//     print('Invalid operation code');
-//   }
+  // Traversing - accessing the elements fo the list
 
+  // Reading
+  for( int i = 0; i < studentList.length; i++ ){
+    print(studentList[i]);
+  }
+
+  // for in loop - for each loop
+  // used with list
+  print('****************************');
+
+  for( String s in studentList){
+    print(s);
+  }
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
