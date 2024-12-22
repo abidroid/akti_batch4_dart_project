7c6bf5e9cc93a47e6704124866805de802709163
Abid <abidroid1@gmail.com>
2024-12-22 11:46:17 +0500

    classes

--- bin/akti_batch4_dart_project.dart
+++ bin/akti_batch4_dart_project.dart
@@ -1,49 +1,80 @@
-// function define
-// user defined function
+void displayStars()
+{
+  print("*******************");
+}
 
-import 'dart:math';
+void main( )
+{
+  displayStars();
+  // int, double, String, bool, List, Map
 
-void printStars() => print('****************************************');
+  Student hamza;
+
+  // instantiate
+
+  hamza = Student();
+  hamza.name = "Hamza Riaz";
+  hamza.mobile = "03838383838";
+  hamza.course = "Flutter";
+
+  hamza.display();
+
+  Student jawad = Student(); // instantiate
+  displayStars();
+  jawad.name = "Jawad Ali";
+  jawad.mobile = "083838383";
+  jawad.course = "Flutter";
+  jawad.display();
+
+  Teacher abid;
+  abid = Teacher();
+  abid.name = "Muhammad Abid";
+  abid.mobile = "03329964494";
+  abid.subject = "Flutter";
+
+}
+
+// user defined Data types
+class Student
+{
+  // variables
+  // data members
+  late String name;
+  late String mobile;
+  late String course;
 
 
-// named parameters
-void printTable( { required int number, required int range }){
-  for( int i = 1; i <= range; i++ )
+  // method
+  void display()
   {
-    print('$number * $i = ${number * i}');
+    print("Name $name");
+    print("Mob  $mobile");
+    print("Course $course");
   }
 }
 
-double square( double number)
+class Teacher
 {
-  return number * number;
+  late String name;
+  late String mobile;
+  late String subject;
+
+  // method
+  void display()
+  {
+    print("Name $name");
+    print("Mob  $mobile");
+    print("Subject $subject");
+  }
 }
 
-double doubleIt( double number){
-  return number * 2;
-}
-
-double myMax( double x, double y) => ( x > y) ? x : y;
-
-
-
-// Boss
-void main( )
+class Beggar
 {
+  late String name;
+  late String stop;
+  late double dailyIncome;
 
-  // print( sqrt( 9 ));
-  // print( sqrt(-9));
-
-  print(myMax(10, 8));
-  print(max(4, 5));
-  print(min(4, 5));
-  print(pow(2, 4));
-
-}
-
-void sum( int x, int y) // parameters
-{
-  int result = x + y;
-  print('Sum = $result');
-
+  double calculateMonthlyIncome() {
+    return dailyIncome * 30;
+  }
 }
